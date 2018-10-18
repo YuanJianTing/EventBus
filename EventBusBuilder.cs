9fa7aa6c47323162cec1659f0c27f1da950f90da
袁建廷 <jiantingYuan@gmail.com>
2018-10-18 13:18:23 +0800

    Add files via upload

---
+++ EventBusBuilder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ESLDesign.eTagTech.SDK.Events
+{
+    public class EventBusBuilder
+    {
+        internal SynchronizationContext context;
+
+        public EventBusBuilder Context(SynchronizationContext Context)
+        {
+            this.context = Context;
+            return this;
+        }
+
+
+
+
+        internal EventBusBuilder() { }
+
+
+        public EventBus build()
+        {
+            return new EventBus(this);
+        }
+
+    }
+}
---
+++ EventMethodAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESLDesign.eTagTech.SDK.Events
+{
+
+    /// <summary>
+    /// 自定义特性 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class EventMethodAttribute : Attribute
+    {
+        public ThreadMode ThreadMode { get; set; } = ThreadMode.None;
+
+    }
+
+    public enum ThreadMode
+    {
+        None=0,
+        MAIN = 1,
+        ASYNC=2
+    }
+
+}
